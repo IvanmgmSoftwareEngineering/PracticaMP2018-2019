a9687e78d267341870355eb5f1035b6921dae2ae
markosolab <33898449+markosolab@users.noreply.github.com>
2019-05-14 17:50:58 +0200

    Add files via upload
    Clase Controlado

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -5,10 +5,68 @@
  */
 package Controlador;
 
-/**
- *
- * @author img
- */
+import Modelo.GameModel;
+
+
 public class Controlador {
-    
+
+    private GameModel modelo;
+
+
+    public Controlador (GameModel modelo){
+        this.modelo = modelo;
+    }
+
+    public void start(int idVentana,String colorSerpiente, int velocidadSerpiente, String nombreCliente) {
+        modelo.start(idVentana,colorSerpiente, velocidadSerpiente, nombreCliente);
+    }
+
+    public void pause() {
+        modelo.pause();
+    }
+
+    public void reanudar() {
+        modelo.reanudar();
+    }
+
+    public void reiniciar() {
+        modelo.reiniciar();
+    }
+
+    public void finalizarJuego() {
+        modelo.finalizarJuego();
+    }
+
+    public void girarDerecha(int idVentana) {
+        modelo.girarDerecha(idVentana);
+    }
+
+    public void girarArriba(int idVentana) {
+        modelo.girarArriba(idVentana);
+    }
+
+    public void girarAbajo(int idVentana) {
+        modelo.girarAbajo(idVentana);
+    }
+
+    public void girarIzquierda(int idVentana) {
+        modelo.girarIzquierda(idVentana);
+    }
+
+
+    //public void crecerSerpiente(Direccion direccion, int tamSerpiente) {
+    // modelo.creceSerpiente(direccion, tamSerpiente);
+    //}
+
+    public void girarArriba() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void girarIzquierda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+
 }
+
