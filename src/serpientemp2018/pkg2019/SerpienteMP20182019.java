86472931d114436e22c1f4b2f3c07d16574ee16e
IvanmgmSoftwareEngineering <ivanmgm88@gmail.com>
2019-05-14 15:25:58 +0200

    Creación del proyecto

---
+++ src/serpientemp2018/pkg2019/SerpienteMP20182019.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serpientemp2018.pkg2019;
+
+/**
+ *
+ * @author img
+ */
+public class SerpienteMP20182019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
